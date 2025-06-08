bbb9b492211266ed74b0a6c4a3a185246930d69c
Steve <stevedsimkins@gmail.com>
2025-06-07 20:47:58 -0400

    chore: Updated vite config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,16 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@client": path.resolve(__dirname, "./src"),
-      "@server": path.resolve(__dirname, "../server/src"),
-      "@shared": path.resolve(__dirname, "../shared/src"),
-      "@": path.resolve(__dirname, "./src")
-    }
-  }
-})
+	plugins: [react(), tailwindcss()],
+	base: import.meta.env.VITE_BASE_URL || "/",
+	resolve: {
+		alias: {
+			"@client": path.resolve(__dirname, "./src"),
+			"@server": path.resolve(__dirname, "../server/src"),
+			"@shared": path.resolve(__dirname, "../shared/src"),
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
+});
