aca769956bc566eb2f2866329678de8c928f1c32
Steve <stevedsimkins@gmail.com>
2025-06-07 21:06:22 -0400

    chore: Testing different path

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 
 export default defineConfig({
 	plugins: [react(), tailwindcss()],
-	base: process.env.VITE_BASE_URL || "/",
+	base: "/",
 	resolve: {
 		alias: {
 			"@client": path.resolve(__dirname, "./src"),
